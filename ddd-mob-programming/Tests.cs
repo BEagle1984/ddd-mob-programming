7eb478fba1978c6114a9b4a9492b0c122465b417
BEagle1984 <aquilinis@outlook.com>
2021-10-07 11:02:57 +0200

    initial commit

---
+++ ddd-mob-programming/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace ddd_mob_programming
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
